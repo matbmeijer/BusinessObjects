57317f31b0bce6ece0cf2abc15099b05037ef351
matbmeijer <matthias@brennotten.net>
2020-05-17 14:25:23 +0200

    First real commit

---
+++ R/functions.R
@@ -0,0 +1,66 @@
+logon_body <- function(username, password, authentication_type){
+  body<-'<attrs xmlns="http://www.sap.com/rws/bip">
+  <attr name="password" type="string">%s</attr>
+  <attr name="auth" type="string" possibilities="secEnterprise,secLDAP,secWinAD,secSAPR3">%s</attr>
+  <attr name="userName" type="string">%s</attr>
+  </attrs>'
+  return(sprintf(body, password, authentication_type, username))
+}
+
+#' @title Logs into SAP Business Objects
+#' @description Logs into SAP Business Objects and saves the SAP Business Objects API token into the System Environment
+#' @param domain SAP Business Objects domain to log into
+#' @param username SAP Business Objects username
+#' @param password SAP Business Objects password
+#' @param authentication_type Athentication Type to logon into SAP Business Objects. Defaults to \code{secLDAP}. Must be one of the following:
+#' \itemize{
+#' \item secLDAP
+#' \item secEnterprise
+#' \item secWinAD
+#' \item secSAPR3
+#' }
+#' @return Does not return any object, only a success message, as SAP API token is saved into the environment to work with further functions.
+#' @author Matthias Brenninkmeijer -
+#'   \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @section Warning: Please be careful when dealing with API keys and other
+#'   secrets & tokens - keep them private and do not publish them.
+#' @examples
+#' \dontrun{
+#' log_on(domain="YOUR_DOMAIN",
+#'        username="YOUR_USERNAME",
+#'        password="YOUR_PASSWORD",
+#'        authentication_type="secLDAP")
+#' }
+#' @export
+
+log_on <- function(domain, username, password, authentication_type="secLDAP"){
+  # GET SAP Cookie
+  url <- httr::modify_url(domain, path = list("biprws", "logon", "long"))
+  cookie_request <- httr::GET(url)
+  cookies <- as.character(cookie_request$cookies$value)
+  names(cookies) <- cookie_request$cookies$name
+  body <- logon_body(username, password, authentication_type)
+  
+  # POST request to obtain SAP Token
+  get_token_request <- httr::POST(url, 
+                            body = body,
+                            httr::content_type("application/xml"),
+                            httr::add_headers(c("Accept"="application/json")),
+                            httr::set_cookies(cookies))
+  # Ensure request is in json format
+  if (httr::http_type(get_token_request) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  # Stop if errors
+  if (httr::http_error(get_token_request)) {
+    stop(sprintf("Error Code %s - %s",
+                 get_token_request$status_code,
+                 httr::http_status(get_token_request$status_code)$message),
+         call. = FALSE)
+  }
+  
+  # Set token to environment variable
+  token<-c("X-SAP-LogonToken"=get_token_request$headers[["x-sap-logontoken"]])
+  Sys.setenv("x_sap_logontoken"=token)
+  message("Logon process succesfull. SAP Logon Token saved into system environment.")
+}
